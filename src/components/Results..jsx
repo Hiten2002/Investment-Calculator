1b1499e2b1d74ac471a0d9ac1aecff164e540008
Hiten2002 <164162110+Hiten2002@users.noreply.github.com>
2024-10-17 15:03:02 +0530

    Investment Calculator Code Upload

---
+++ src/components/Results..jsx
@@ -0,0 +1,42 @@
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
+
+export default function Results({ input }) {
+  const resultData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultData[0].valueEndOfYear - resultData[0].annualInvestment;
+
+  console.log(resultData);
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map((yearData) => {
+          const totalinterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+
+          const totalAmountInvested = yearData.valueEndOfYear - totalinterest;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalinterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
